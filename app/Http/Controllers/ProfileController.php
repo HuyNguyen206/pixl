c21cd9607b896afb5e6fc33aa7dedabd0e8bf591
lehuy <lehuy.nguyen@ekino.com>
2026-07-25 16:01:51 +0700

    Before 60

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Profile;
 use App\Queries\ProfilePostQuery;
 use App\Queries\RepliesQuery;
+use Illuminate\Database\Eloquent\Builder;
 use Inertia\Inertia;
 
 class ProfileController extends Controller
@@ -12,6 +13,9 @@ class ProfileController extends Controller
     public function show(Profile $profile)
     {
         $profile->loadCount(['followers', 'followings']);
+        $profile->loadExists([
+            'followers as is_follow' => fn (Builder $query) => $query->where('follows.follower_profile_id', auth()->user()?->profile->id)
+        ]);
 
         $posts = ProfilePostQuery::for($profile, auth()->user()?->profile)->get();
 
--- resources/js/Pages/Components/ProfileHeader.vue
+++ resources/js/Pages/Components/ProfileHeader.vue
@@ -30,7 +30,8 @@ const isActive = (name) => currentPath.value === normalize(route(name, props.pro
                 <a :href="route('profiles.show', profile)" class="text-pixl-light/40">{{ '@' + profile.handle }}</a>
             </div>
         </div>
-        <a href="#" class="bg-pixl-light/10 border border-transparent px-4 py-0.5">Edit profile</a>
+        <Link :href="route('posts.follow-toggle', profile)" method="post" class="bg-pixl-light/10 text-pixl border border-transparent px-4 py-0.5 mr-4"
+        >{{profile.is_follow ? 'Unfollow' : 'Follow'}}</Link>
     </div>
     <!--       User profile end-->
 
